0717cdf10547b7290761f9d157c14e416f58a71e
Derek Croft <derekcroft@gmail.com>
2009-03-28 16:35:55 -0400

    Modified items migration and added a gitignore

--- db/migrate/20090328193750_create_items.rb
+++ db/migrate/20090328193750_create_items.rb
@@ -1,7 +1,8 @@
 class CreateItems < ActiveRecord::Migration
   def self.up
     create_table :items do |t|
-
+      t.string :name, :description
+      t.integer :category_id, :aisle_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,40 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20090328193954) do
+
+  create_table "aisles", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "items", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "category_id"
+    t.integer  "aisle_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stores", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
