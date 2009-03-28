2c14c85f69ef1cb460292afafaee3109fd97f8de
srini <srini@srini-laptop.(none)>
2009-03-28 17:48:30 -0400

    Remove aisles.

--- db/migrate/20090328193750_create_items.rb
+++ db/migrate/20090328193750_create_items.rb
@@ -3,7 +3,6 @@ class CreateItems < ActiveRecord::Migration
     create_table :items do |t|
     t.float :price
     t.integer :category_id
-    t.integer :aisle_id
     t.string :description
     t.string :unit_of_measure
       t.timestamps
