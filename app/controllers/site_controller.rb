02a7a929a0edf84f812ae96822e5b539b3e7069f
Derek Croft <derekcroft@gmail.com>
2009-04-07 16:59:05 -0400

    Added auto complete on items to main search field.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,7 @@
 class SiteController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => [:auto_complete_for_item_name]
+  auto_complete_for :item, :name
+
   def index
   end
 end
