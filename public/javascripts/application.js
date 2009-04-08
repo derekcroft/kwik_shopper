3368c505a2c21852ff386bafaaa426a0c6a1b402
Derek Croft <derekcroft@gmail.com>
2009-04-08 17:54:22 -0400

    Added remove feature from shopping cart.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,20 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+var Iterators = function() {
+  var tags = "div p span ul ol li span form input select textarea h1 h2 h3 h4 h5 h6 dl dt em strong";
+  var methods = {};
+  $A(tags.split(' ')).each(function(tag) {
+    methods["each" + tag.charAt(0).toUpperCase() + tag.substring(1)] = function(element, iterator) {
+      element = $(element);
+      element.cleanWhitespace();
+      $A(element.getElementsByTagName(tag)).each(iterator);
+    }
+  });
+
+  Element.addMethods(methods);
+}();
+
 const DEFAULT_SEARCH_TEXT = 'Enter a product to search for';
 
 function changeDefaultFieldOnFocus(elem) {
@@ -15,10 +29,26 @@ function changeDefaultFieldOnBlur(elem) {
   }
 }
 
+function packShoppingCartIds() {
+  var i=0
+  $('shopping-list-content').eachLi(function(item) {
+    item.setAttribute('item_id', i++);
+  });
+  return i;
+}
+
+function removeItem(itemId) {
+  $('shopping-list-content').select('[item_id="'+itemId+'"]').each(function(item) {
+    Element.remove(item)
+  });
+ return packShoppingCartIds();
+}
+
 function insertItemAndHighlight(listId, itemName) {
-   var list_item_html = '<li>'+itemName+'</li>'
+   var list_item_html = '<li><a href="#" onclick="removeItem(ancestors(this)[0].readAttribute(\'item_id\')); return false;">'+itemName+'</a></li>'
    Element.insert(listId, { bottom: list_item_html })
    new Effect.Highlight(listId,{});
+   return packShoppingCartIds();
 }
 
 function clearFieldAndFocus(field) {
