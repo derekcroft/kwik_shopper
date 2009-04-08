98f6eb9a1ba138b8b78423bace5b96650111b9ce
Derek Croft <derekcroft@gmail.com>
2009-04-08 10:38:22 -0400

    Externalized default field Javascript functions and add to cart functions to application.js.

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -2,7 +2,7 @@ module SiteHelper
   def link_to_submit_image
     link_to_function(image_tag("search-submit.png"), nil, :class => "btn") do |page|
       page.hide('getting-started')
-      page.insert_html(:bottom, 'shopping-list-content', "<li>Item</li>")
+      page.call('addItemToShoppingCart')
     end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+const DEFAULT_SEARCH_TEXT = 'Enter a product to search for';
+
+function changeDefaultFieldOnFocus(elem) {
+  if (elem.value == DEFAULT_SEARCH_TEXT) {
+    elem.value = ''
+  }
+}
+
+function changeDefaultFieldOnBlur(elem) {
+  if (elem.value == '') {
+    elem.value = DEFAULT_SEARCH_TEXT
+  }
+}
+
+function addItemToShoppingCart() {
+  var search_box_value = document.getElementById('item_name').value
+  var shopping_cart_item_html = '<li>'+search_box_value+'</li>'
+  Element.insert('shopping-list-content', { bottom: shopping_cart_item_html })
+}
