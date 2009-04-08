9c14a477d96e041074cf7a0c8ad6bcb6eb243509
Derek Croft <derekcroft@gmail.com>
2009-04-08 00:42:36 -0400

    Externalized search submit button functionality to helper.

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,8 @@
 module SiteHelper
+  def link_to_submit_image
+    link_to_function(image_tag("search-submit.png"), nil, :class => "btn") do |page|
+      page.hide('getting-started')
+      page.insert_html(:bottom, 'shopping-list-content', "<li>Item</li>")
+    end
+  end
 end
