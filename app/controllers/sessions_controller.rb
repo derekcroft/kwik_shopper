0d282ae054ba07a6bc048c202a2430ace4e4dfe3
Derek Croft <derekcroft@gmail.com>
2009-04-14 16:43:14 -0400

    Added basic login functionality to the index page -- no UI confirmation of successful or unsuccessful login yet.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,12 @@ class SessionsController < ApplicationController
 
   def create
     logout_keeping_session!
-    user = User.authenticate(params[:login], params[:password])
+    user = User.authenticate(params[:username], params[:password])
     if user
       # Protects against session fixation attacks, causes request forgery
       # protection if user resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
-      # reset_session
+      #reset_session
       self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
       flash[:notice] = "Logged in successfully"
     else
       note_failed_signin
-      @login       = params[:login]
+      @login       = params[:username]
       @remember_me = params[:remember_me]
       render :action => 'new'
     end
@@ -37,7 +37,7 @@ class SessionsController < ApplicationController
 protected
   # Track failed login attempts
   def note_failed_signin
-    flash[:error] = "Couldn't log you in as '#{params[:login]}'"
-    logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
+    flash[:error] = "Couldn't log you in as '#{params[:username]}'"
+    logger.warn "Failed login for '#{params[:username]}' from #{request.remote_ip} at #{Time.now.utc}"
   end
 end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,10 @@
 class SiteController < ApplicationController
+  include AuthenticatedSystem
+
   skip_before_filter :verify_authenticity_token, :only => [:auto_complete_for_item_name]
   auto_complete_for :item, :name
 
   def index
+   
   end
 end
