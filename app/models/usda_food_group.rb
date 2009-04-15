4dd268c78ecde34b3e20b9859d93f20eb3993e8d
Derek Croft <derekcroft@gmail.com>
2009-04-15 17:10:08 -0400

    Split from_csv_quickly code into methods and used table_name as the CSV file prefix.

--- app/models/usda_food_group.rb
+++ app/models/usda_food_group.rb
@@ -1,10 +1,15 @@
 class UsdaFoodGroup < ActiveRecord::Base
   has_many :usda_foods
 
+  def self.csv_file_name
+    csv_directory = "#{RAILS_ROOT}/db"
+    "#{csv_directory}/#{self.table_name}.csv"
+  end
+
   def self.from_csv_quickly
     self.transaction do
       self.destroy_all
-      FasterCSV.foreach("#{RAILS_ROOT}/db/usda_food_groups.csv", :headers => :first_row) do |row|
+      FasterCSV.foreach(self.csv_file_name, :headers => :first_row) do |row|
         self.create(row.to_hash)
       end
     end
